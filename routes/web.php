1919d355ccaaf1982532b3bb60a1a24c657aae58
mostafa jahani <mm.mostafa.mm@gmail.com>
2024-12-16 22:06:14 +0330

    initial project

---
+++ routes/web.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+require __DIR__.'/auth.php';
+require __DIR__.'/admin.php';
+require __DIR__.'/site.php';
