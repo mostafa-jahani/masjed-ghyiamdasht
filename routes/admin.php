fe6a0fa3dd9e500304bfb6e21ab8c517cc152af5
Mostafa Jahani <mm.mostafa.mm@gmail.com>
2024-12-17 22:17:45 +0330

    implement admin layouts

--- routes/admin.php
+++ routes/admin.php
@@ -5,8 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('admin.index');
+})->name('dashboard');
+
+
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
